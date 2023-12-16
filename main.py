00416ccae0607c222b91f51a8c1a0dd839ed9a48
이주형 JuhyeongLee <smilebank7@naver.com>
2023-12-16 21:51:37 +0900

    [UPDATE] swagger

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-        
+    title = "Sans",
+    description = "API description",
+    version = "0.1.0",
+    docs_url = "/docs",
+    redoc_url = "/redoc"
 )
 
 
